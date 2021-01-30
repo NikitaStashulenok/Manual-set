c2592f48b3ca7d819115733708c701943246bb29
Nikita Stashulenok <nstashylenok@mail.ru>
2021-01-30 16:54:43 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace My_Set
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var set1 = new SuperSet<int>(new int[] { 1, 2, 3, 4, 5 });
+            var set2 = new SuperSet<int>(new int[] { 4, 5, 6, 7, 8 });
+            var set3 = new SuperSet<int>(new int[] { 2, 3, 4 });
+
+            Console.Write("Union: ");
+            foreach(var item in set1.Union(set2))
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("Intersection: ");
+            foreach (var item in set1.Intersection(set2))
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("Difference A \\ B: ");
+            foreach (var item in set1.Difference(set2))
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("Difference B \\ A: ");
+            foreach (var item in set2.Difference(set1))
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("A Subset C: ");
+            Console.WriteLine(set1.Subset(set3));
+            Console.WriteLine();
+
+            Console.Write("C Subset A: ");
+            Console.WriteLine(set3.Subset(set1));
+            Console.WriteLine();
+
+            Console.Write("C Subset B: ");
+            Console.WriteLine(set3.Subset(set2));
+            Console.WriteLine();
+
+            Console.Write("Symetric Difference: ");
+            foreach (var item in set1.SymetricDifference(set2))
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SuperSet.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace My_Set
+{
+    public class SuperSet<T> : IEnumerable
+    {
+        private readonly List<T> items = new List<T>();
+        public int Count => items.Count;
+        public SuperSet() { }
+
+        public SuperSet(T item)
+        {
+            items.Add(item);
+        }
+
+        public SuperSet(IEnumerable<T> items)
+        {
+            this.items = items.ToList();
+        }
+
+        public void Add(T item)
+        {
+            //LINQ
+
+            if (items.Contains(item))
+            {
+                return;
+            }
+
+            //Manual Add
+
+            //foreach (var i in items)
+            //{
+            //    if (i.Equals(item))
+            //    {
+            //        return
+            //    }
+            //}
+            items.Add(item);
+        }
+
+        public void Remove(T item)
+        {
+            items.Remove(item);
+        }
+
+        public SuperSet<T> Union(SuperSet<T> set)
+        {
+            //LINQ
+
+            return new SuperSet<T>(items.Union(set.items));
+
+            //Manual Union
+
+            //SuperSet<T> result = new SuperSet<T>();
+
+            //foreach (var item in items)
+            //{
+            //    result.Add(item);
+            //}
+
+            //foreach (var item in set.items)
+            //{
+            //    result.Add(item);
+            //}
+        }
+
+        public SuperSet<T> Intersection(SuperSet<T> set)
+        {
+            //LINQ
+
+            return new SuperSet<T>(set.items.Intersect<T>(items));
+
+            //Manual Intersection
+
+            //var result = new SuperSet<T>();
+            //SuperSet<T> big;
+            //SuperSet<T> small;
+
+            //if (Count >= set.Count)
+            //{
+            //    big = this;
+            //    small = set;
+            //}
+            //else
+            //{
+            //    big = set;
+            //    small = this;
+            //}
+            //foreach (var item1 in small.items)
+            //{
+            //    foreach (var item2 in big.items)
+            //    {
+            //        if (item1.Equals(item2))
+            //        {
+            //            result.Add(item1);
+            //            break;
+            //        }
+            //    }
+            //}
+        }
+
+        public SuperSet<T> Difference(SuperSet<T> set)
+        {
+            //LINQ
+
+            return new SuperSet<T>(items.Except(set.items));
+
+            //Manual Difference
+
+            //var result = new SuperSet<T>(items);
+
+            //foreach(var item in set.items)
+            //{
+            //    result.Remove(item);
+            //}
+            
+            //return result;
+        }
+
+        public bool Subset(SuperSet<T> set)
+        {
+            //LINQ
+            return items.All(i => set.items.Contains(i));
+
+            //Manual Subset
+
+            //foreach(var item1 in items)
+            //{
+            //    var equals = false;
+
+            //    foreach(var item2 in set.items)
+            //    {
+            //        if(item1.Equals(item2))
+            //        {
+            //            equals = true;
+            //            break;
+            //        }
+            //    }
+
+            //    if(!equals)
+            //    {
+            //        return false;
+            //    }
+            //}
+            //return true;
+        }
+
+        public SuperSet<T> SymetricDifference(SuperSet<T> set)
+        {
+            //LINQ
+
+            return new SuperSet<T>(items.Except(set.items).Union(set.items.Except(items)));
+
+            //Manual Difference
+
+            //var result = new SuperSet<T>();
+
+            //foreach (var item1 in items)
+            //{
+            //    var equals = false;
+
+            //    foreach (var item2 in set.items)
+            //    {
+            //        if(item1.Equals(item2))
+            //        {
+            //            equals = true;
+            //            break;
+            //        }
+            //    }
+
+            //    if(equals == false)
+            //    {
+            //        result.Add(item1);
+            //    }
+            //}
+
+            //foreach (var item1 in set.items)
+            //{
+            //    var equals = false;
+
+            //    foreach (var item2 in items)
+            //    {
+            //        if (item1.Equals(item2))
+            //        {
+            //            equals = true;
+            //            break;
+            //        }
+            //    }
+
+            //    if (equals == false)
+            //    {
+            //        result.Add(item1);
+            //    }
+            //}
+
+            //return result;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return items.GetEnumerator();
+        }
+    }
+}
